fd6dc4d40924cb308373a74fcf22628c7dafb57d
Cameron Noemdo <219115443@mycput.ac.za>
2022-10-23 19:26:32 +0200

    ID generator and email validator

--- src/main/java/za/ac/cput/generic/GenericHelper.java
+++ src/main/java/za/ac/cput/generic/GenericHelper.java
@@ -1,4 +1,18 @@
 package za.ac.cput.generic;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.util.UUID;
+
 public class GenericHelper {
+    public static String generateID() {
+        return UUID.randomUUID().toString();
+    }
+
+    // email validator
+    public static boolean emailValidation(String email) {
+        if(!EmailValidator.getInstance().isValid(email))
+            throw new IllegalArgumentException("Invalid email");
+        return EmailValidator.getInstance().isValid(email);
+    }
 }
