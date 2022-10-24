611fd0131355b156f8b82665bc5ae66e3a76a88e
lefukumeke123 <kumekel3310@gmail.com>
2022-10-24 08:04:14 +0200

    System Admin

--- src/main/java/za/ac/cput/factory/SystemAdminFactory.java
+++ src/main/java/za/ac/cput/factory/SystemAdminFactory.java
@@ -1,4 +1,30 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.SystemAdmin;
+import za.ac.cput.generic.GenericHelper;
+
 public class SystemAdminFactory {
+
+    public static SystemAdmin createSystemAdmin(String adminName, String adminEmail)
+    {
+
+
+        String adminID = GenericHelper.generateUniqueIntId();
+
+        SystemAdmin systemAdmin = new SystemAdmin.Builder()
+                .setAdminID(adminID)
+                .setAdminName(adminName)
+                .setAdminEmail(adminEmail)
+                .build();
+        return systemAdmin;
+    }
+    public static SystemAdmin updateSystemAdmin(String adminID , String adminName, String adminEmail)
+    {
+        return new SystemAdmin.Builder()
+                .setAdminID(adminID)
+                .setAdminName(adminName)
+                .setAdminEmail(adminEmail)
+                .build();
+    }
+
 }
--- src/main/java/za/ac/cput/views/systemAdmin/ReadSystemAdmin.java
+++ src/main/java/za/ac/cput/views/systemAdmin/ReadSystemAdmin.java
@@ -1,7 +1,7 @@
 package za.ac.cput.views.systemAdmin;
+
 import za.ac.cput.client.SystemAdminHttpClient;
 import za.ac.cput.entity.SystemAdmin;
-import za.ac.cput.factory.SystemAdminFactory;
 import za.ac.cput.views.SystemAdminMenu;
 
 import javax.swing.*;
@@ -9,37 +9,36 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CreateSystemAdmin extends JFrame implements ActionListener {
+public class ReadSystemAdmin extends JFrame implements ActionListener {
 
     //Attributes
     private JPanel northPanel, centerPanel, southPanel;
-    private JLabel lblHeading, lblAdminId, lblAdminId1, lblName, lblEmailAddress;
-    private  JTextField  txtName,  txtEmailAddress;
-    private JButton btnCreate, btnExit;
+    private JLabel lblHeading, lblAdminId, lblName, lblEmail;
+    private JLabel lblName1, lblEmail1;
+    private JTextField txtAdminId;
+    private JButton btnRead, btnClear, btnExit;
     private Font ftHeading, ftText, ftTextBold;
-    private JLabel emptySpace1, emptySpace2, emptySpace3, emptySpace4, emptySpace5, emptySpace6, emptySpace7, emptySpace8, emptySpace9, emptySpace10, emptySpace11;
+    private JLabel emptySpace1, emptySpace2, emptySpace3, emptySpace4, emptySpace5, emptySpace6, emptySpace7, emptySpace8, emptySpace9, emptySpace10, emptySpace11, emptySpace12;
 
-    public CreateSystemAdmin()
+    public ReadSystemAdmin()
     {
-        super("Create System Admin Screen ");
+        super("Read System Admin");
 
         northPanel = new JPanel();
         centerPanel = new JPanel();
         southPanel = new JPanel();
 
-
-        lblHeading = new JLabel("Create System Admin", SwingConstants.CENTER);
-        lblAdminId = new JLabel("System Admin ID: ", SwingConstants.RIGHT);
+        lblHeading = new JLabel("Read System Admin", SwingConstants.CENTER);
+        lblAdminId = new JLabel("Admin ID: ", SwingConstants.RIGHT);
         lblName = new JLabel("Admin Name: ", SwingConstants.RIGHT);
-        lblEmailAddress = new JLabel("Admin Email Address: ", SwingConstants.RIGHT);
+        lblEmail = new JLabel("Admin Email: ", SwingConstants.RIGHT);
 
+        txtAdminId = new JTextField();
+        lblName1 = new JLabel("");
+        lblEmail1 = new JLabel("");
 
-        lblAdminId1 = new JLabel("Auto is Generated...");
-        txtName = new JTextField();
-        txtEmailAddress = new JTextField();
-
-
-        btnCreate = new JButton("Create");
+        btnRead = new JButton("Read");
+        btnClear = new JButton("Clear");
         btnExit = new JButton("Exit");
 
         ftHeading = new Font("Segoe UI Black", Font.PLAIN, 28);
@@ -57,15 +56,15 @@ public class CreateSystemAdmin extends JFrame implements ActionListener {
         emptySpace9 = new JLabel();
         emptySpace10 = new JLabel();
         emptySpace11 = new JLabel();
+        emptySpace12 = new JLabel();
     }
 
     public void setGui()
     {
         //Add Gridlayout to panels
         northPanel.setLayout(new FlowLayout());
-        centerPanel.setLayout(new GridLayout(4,6));
-        southPanel.setLayout(new GridLayout(2,2));
-
+        centerPanel.setLayout(new GridLayout(3,3));
+        southPanel.setLayout(new GridLayout(2,3));
 
         //Set font
         lblHeading.setFont(ftHeading);
@@ -73,41 +72,38 @@ public class CreateSystemAdmin extends JFrame implements ActionListener {
 
         lblAdminId.setFont(ftTextBold);
         lblName.setFont(ftTextBold);
-        lblEmailAddress.setFont(ftTextBold);
+        lblEmail.setFont(ftTextBold);
 
-
-        btnCreate.setFont(ftTextBold);
+        btnRead.setFont(ftTextBold);
         btnExit.setFont(ftTextBold);
 
-        lblAdminId1.setFont(ftText);
-        txtName.setFont(ftText);
-
-        txtEmailAddress.setFont(ftText);
-
+        txtAdminId.setFont(ftText);
+        lblName1.setFont(ftText);
+        lblEmail1.setFont(ftText);
 
         //Add components to panels
         northPanel.add(lblHeading);
-
+        //northPanel.setBackground(Color.decode("#4863A0"));
 
         centerPanel.add(lblAdminId);
-        centerPanel.add(lblAdminId1);
+        centerPanel.add(txtAdminId);
         centerPanel.add(emptySpace1);
 
         centerPanel.add(lblName);
-        centerPanel.add(txtName);
+        centerPanel.add(lblName1);
         centerPanel.add(emptySpace2);
 
-        centerPanel.add(lblEmailAddress);
-        centerPanel.add(txtEmailAddress);
+        centerPanel.add(lblEmail);
+        centerPanel.add(lblEmail1);
         centerPanel.add(emptySpace3);
-
-
 
         //centerPanel.setBackground(Color.decode("#CECECE"));
 
         southPanel.add(emptySpace10);
         southPanel.add(emptySpace11);
-        southPanel.add(btnCreate);
+        southPanel.add(emptySpace12);
+        southPanel.add(btnRead);
+        southPanel.add(btnClear);
         southPanel.add(btnExit);
         //southPanel.setBackground(Color.decode("#CECECE"));
 
@@ -117,48 +113,50 @@ public class CreateSystemAdmin extends JFrame implements ActionListener {
         this.add(southPanel, BorderLayout.SOUTH);
 
         //Add action listener to buttons | mouse listener to hyperlink
-        btnCreate.addActionListener(this);
+        btnRead.addActionListener(this);
+        btnClear.addActionListener(this);
         btnExit.addActionListener(this);
 
         //Frame
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.pack();
-        this.setSize(640, 280);
+        this.setSize(640, 240);
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Create")) {
-            String name = txtName.getText().trim().toString();
-            String emailAddress = txtEmailAddress.getText().trim().toString();
+        if(e.getActionCommand().equals("Read"))
+        {
+            String adminId = txtAdminId.getText().trim().toString();
 
-
-            if (name.isEmpty() ||  emailAddress.isEmpty())
+            if(adminId == null)
             {
-                JOptionPane.showMessageDialog(null, "Please fill in all information to create a profile.");
+                JOptionPane.showMessageDialog(null, "Please enter a valid ID.");
             }
+            else{
+                SystemAdmin result = SystemAdminHttpClient.read(adminId);
 
-            else {
+                if(result != null)
+                {
+                    JOptionPane.showMessageDialog(null, "System admin exist with ID of: " + adminId);
 
-                SystemAdmin createSystemAdmin = SystemAdminFactory.createSystemAdmin(name, emailAddress);
-
-                SystemAdmin result = SystemAdminHttpClient.create(createSystemAdmin);
-
-                if (result != null) {
-                    JOptionPane.showMessageDialog(null, "You have successfully created your profile!.");
-
-                    txtName.setText("");
-                    txtEmailAddress.setText("");
-
-
-                    txtName.requestFocus();
-                } else {
-                    JOptionPane.showMessageDialog(null, "There was an error creating a profile.");
+                    lblName1.setText(result.getAdminName());
+                    lblEmail1.setText(result.getAdminEmail());
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "No System Admin exists with ID of: " + adminId);
                 }
             }
         }
+        else if(e.getActionCommand().equals("Clear"))
+        {
+            txtAdminId.setText("");
+            lblName1.setText("");
+            lblEmail1.setText("");
 
+            txtAdminId.requestFocus();
+        }
         else if(e.getActionCommand().equals("Exit"))
         {
             new SystemAdminMenu().setGui();
@@ -167,6 +165,8 @@ public class CreateSystemAdmin extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        new CreateSystemAdmin().setGui();
+        new ReadSystemAdmin().setGui();
     }
 }
+
+
