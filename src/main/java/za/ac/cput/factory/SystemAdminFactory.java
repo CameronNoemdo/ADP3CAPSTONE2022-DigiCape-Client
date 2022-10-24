8ee88208bfb33cfd57f6563bd9a76b5994fa2f00
lefukumeke123 <kumekel3310@gmail.com>
2022-10-24 09:13:42 +0200

    System Admin

--- src/main/java/za/ac/cput/factory/SystemAdminFactory.java
+++ src/main/java/za/ac/cput/factory/SystemAdminFactory.java
@@ -11,12 +11,11 @@ public class SystemAdminFactory {
 
         String adminID = GenericHelper.generateID();
 
-        SystemAdmin systemAdmin = new SystemAdmin.Builder()
+        return new SystemAdmin.Builder()
                 .setAdminID(adminID)
                 .setAdminName(adminName)
                 .setAdminEmail(adminEmail)
                 .build();
-        return systemAdmin;
     }
     public static SystemAdmin updateSystemAdmin(String adminID , String adminName, String adminEmail)
     {
